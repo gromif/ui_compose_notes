6717ed727ff7fb8e9fb373ba8e2a3b886441aa41
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-22 15:59:21 +0200

    Add LibraryComposeConventionPlugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,16 @@
 plugins {
     alias(libs.plugins.astracrypt.android.library)
+    alias(libs.plugins.astracrypt.android.library.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
-    alias(libs.plugins.compose.compiler)
 }
 
 android {
     namespace = "com.nevidimka655.notes"
-
-    defaultConfig {
-        buildFeatures.compose = true
-    }
 }
 
 dependencies {
     api(projects.domain.notes)
-    api(projects.ui.composeCore)
 
     // Hilt
     ksp(libs.hilt.android.compiler)
