b308274e4cc8c6d3b0a8efba734e2cad1576d570
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-26 20:01:44 +0200

    Remove data from feature layer

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,6 @@ android {
 
 dependencies {
     implementation(projects.domain.notes)
-    implementation(projects.data.notes)
     implementation(projects.di.notes)
 
     implementation(projects.di.core)
