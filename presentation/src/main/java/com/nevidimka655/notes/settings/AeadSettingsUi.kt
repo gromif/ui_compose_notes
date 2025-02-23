0809aec4116c9a6bfc1f205f46d6e820c462f6eb
gromif <alex.kobrys@gmail.com>
2025-02-23 21:42:47 +0200

    fix(notes): correct `PreferencesGroup` padding

--- presentation/src/main/java/com/nevidimka655/notes/settings/AeadSettingsUi.kt
+++ presentation/src/main/java/com/nevidimka655/notes/settings/AeadSettingsUi.kt
@@ -1,6 +1,8 @@
 package com.nevidimka655.notes.settings
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -8,6 +10,7 @@ import com.nevidimka655.astracrypt.resources.R
 import com.nevidimka655.domain.notes.model.AeadMode
 import com.nevidimka655.notes.settings.shared.DatabaseOption
 import com.nevidimka655.ui.compose_core.PreferencesGroup
+import com.nevidimka655.ui.compose_core.theme.spaces
 import io.gromif.crypto.tink.model.AeadTemplate
 
 @Composable
@@ -16,7 +19,10 @@ internal fun AeadSettingsUi(
     aeadMode: AeadMode? = AeadMode.None,
     aeadTemplatesList: List<AeadTemplate> = listOf(),
     onNotesAeadChange: (AeadMode) -> Unit = {},
-) = Column(modifier = modifier) {
+) = Column(
+    modifier = modifier,
+    verticalArrangement = Arrangement.spacedBy(MaterialTheme.spaces.spaceMedium)
+) {
     PreferencesGroup(text = stringResource(id = R.string.notes)) {
         if (aeadMode != null) DatabaseOption(
             aeadMode = aeadMode,
